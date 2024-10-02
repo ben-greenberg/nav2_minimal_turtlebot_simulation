82e02940853413d23d3622a24f1f289ba2c24746
Ben <benrgreenberg@gmail.com>
2024-10-02 10:25:56 -0400

    changes for hallway env

--- nav2_minimal_tb3_sim/launch/spawn_tb3.launch.py
+++ nav2_minimal_tb3_sim/launch/spawn_tb3.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
     robot_name = LaunchConfiguration('robot_name')
     robot_sdf = LaunchConfiguration('robot_sdf')
     pose = {'x': LaunchConfiguration('x_pose', default='-2.00'),
-            'y': LaunchConfiguration('y_pose', default='-0.50'),
+            'y': LaunchConfiguration('y_pose', default='0.00'),
             'z': LaunchConfiguration('z_pose', default='0.01'),
             'R': LaunchConfiguration('roll', default='0.00'),
             'P': LaunchConfiguration('pitch', default='0.00'),
